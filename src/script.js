ea7108ab8eb1f381df756d556a9e5b4d9f027e09
ArifR_ <arifr0751@gmail.com>
2024-08-15 14:15:21 +0700

    new

--- src/script.js
+++ src/script.js
@@ -1,34 +1,60 @@
-const borderClicked = document.getElementById("border-clicked")
-const inputLabel = document.getElementById("lbl-email")
-const inputEmail = document.getElementById("in-email")
-const warning1 = document.getElementById("warning1")
+const borderClicked = document.getElementById("border-clicked");
+const inputLabel = document.getElementById("lbl-email");
+const inputEmail = document.getElementById("in-email");
+const warning1 = document.getElementById("warning1");
+const warning2 = document.getElementById("warning2");
+import validator from "https://cdn.skypack.dev/validator";
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    borderClicked.addEventListener("click",()=>{
-        borderClicked.classList.add("outline-2","outline-fff", "outline", "outline-offset-2")
-        inputEmail.classList.remove('hidden')
-        inputEmail.focus()
-        inputLabel.classList.remove('text-base')
-        inputLabel.classList.add('text-[12px]')
-        warning1.classList.remove("flex")
-        warning1.classList.add("hidden")
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  borderClicked.addEventListener("click", () => {
+    borderClicked.classList.add(
+      "outline-2",
+      "outline-fff",
+      "outline",
+      "outline-offset-2"
+    );
+    inputEmail.classList.remove("hidden");
+    inputEmail.focus();
+    inputLabel.classList.remove("text-base");
+    inputLabel.classList.add("text-[12px]");
+    warning1.classList.remove("flex");
+    warning1.classList.add("hidden");
+    warning2.classList.remove("flex");
+    warning2.classList.add("hidden");
+  });
+});
 
-document.addEventListener("click", ()=>{
-    if(!borderClicked.contains(event.target)){
-        borderClicked.classList.remove("outline-2","outline-fff", "outline", "outline-offset-2")
-        inputEmail.classList.add('hidden')
-        inputLabel.classList.remove('text-[12px]')
-        inputLabel.classList.add('text-base')
-        if(inputEmail.value === ""){
-            warning1.classList.remove("hidden")
-            warning1.classList.add("flex")
-        }else{
-            inputEmail.classList.remove('hidden')
-            inputLabel.classList.remove('text-base')
-            inputLabel.classList.add('text-[12px]')
-        }
+document.addEventListener("click", () => {
+  if (!borderClicked.contains(event.target)) {
+    borderClicked.classList.remove(
+      "outline-2",
+      "outline-fff",
+      "outline",
+      "outline-offset-2"
+    );
+    inputEmail.classList.add("hidden");
+    inputLabel.classList.remove("text-[12px]");
+    inputLabel.classList.add("text-base");
+    warning2.classList.remove("flex");
+    warning2.classList.add("hidden");
+    if (inputEmail.value === "") {
+      warning1.classList.remove("hidden");
+      warning1.classList.add("flex");
+      warning2.classList.remove("flex");
+      warning2.classList.add("hidden");
     }
-})
-
+  }
+  if (inputEmail.value !== "") {
+    warning2.classList.remove("flex");
+    warning2.classList.add("hidden");
+    inputEmail.classList.remove("hidden");
+    inputLabel.classList.remove("text-base");
+    inputLabel.classList.add("text-[12px]");
+    if (!validator.isEmail(inputEmail.value)) {
+      warning1.classList.remove("flex");
+      warning1.classList.add("hidden");
+      warning2.classList.remove("hidden");
+      warning2.classList.add("flex");
+    }
+  }
+});
